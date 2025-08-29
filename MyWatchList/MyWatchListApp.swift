83bd5c58bde4539e99a88867ba0c773f8b450193
Marwen Haouacine <marwen75@gmail.com>
2025-08-29 11:47:09 +0200

    Initial Commit

---
+++ MyWatchList/MyWatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWatchListApp.swift
+//  MyWatchList
+//
+//  Created by Marwen Haouacine on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyWatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
